fbbd6b6bb4f69bc810618d4346f34003f4bee101
Raymond427 <rferrel427@gmail.com>
2017-05-25 22:32:44 -0400

    navbar styled

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,39 +6,41 @@ class Navbar extends Component {
    render() {
       return (
          <nav className="Navbar">
-            <img src={logo} className="Navbar-logo" alt="logo" onClick={() => this._changeCurrentPage("Home")} />
-            <ul className="Navbar-links">
-               <li>
-                  <a href="#" onClick={() => this._changeCurrentPage("Home")}>
-                     Home
-                  </a>
-               </li>
-               <li>
-                  <a href="#" onClick={() => this._changeCurrentPage("UserX")}>
-                     User experience
-                  </a>
-               </li>
-               <li>
-                  <a href="#" onClick={() => this._changeCurrentPage("WhatWeDo")}>
-                     What we do
-                  </a>
-               </li>
-               <li>
-                  <a href="#" onClick={() => this._changeCurrentPage("OurTeam")}>
-                     Our team
-                  </a>
-               </li>
-               <li>
-                  <a href="#" onClick={() => this._changeCurrentPage("OurWork")}>
-                     Our work
-                  </a>
-               </li>
-               <li>
-                  <a href="#" onClick={() => this._changeCurrentPage("ContactUs")}>
-                     Contact us
-                  </a>
-               </li>
-            </ul>
+            <div className="Navbar-wrapper">
+               <img src={logo} className="Navbar-logo" alt="logo" onClick={() => this._changeCurrentPage("Home")} />
+               <ul className="Navbar-links">
+                  <li>
+                     <a href="#" onClick={() => this._changeCurrentPage("Home")}>
+                        Home
+                     </a>
+                  </li>
+                  <li>
+                     <a href="#" onClick={() => this._changeCurrentPage("UserX")}>
+                        User experience
+                     </a>
+                  </li>
+                  <li>
+                     <a href="#" onClick={() => this._changeCurrentPage("WhatWeDo")}>
+                        What we do
+                     </a>
+                  </li>
+                  <li>
+                     <a href="#" onClick={() => this._changeCurrentPage("OurTeam")}>
+                        Our team
+                     </a>
+                  </li>
+                  <li>
+                     <a href="#" onClick={() => this._changeCurrentPage("OurWork")}>
+                        Our work
+                     </a>
+                  </li>
+                  <li>
+                     <a href="#" onClick={() => this._changeCurrentPage("ContactUs")}>
+                        Contact us
+                     </a>
+                  </li>
+               </ul>
+            </div>
          </nav>
       );
    }
